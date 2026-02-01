4d73920dbb8523204e65ce639583057fe8d68334
Seirione <bautistaaironepol5@gmail.com>
2026-02-01 20:13:23 +0800

    Changes on the Notes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -15,4 +15,34 @@ class Home extends BaseController
           // dd($data);
        // var_dump(compact('data'));
     }
+
+
+    public function get_from_form(){
+        //Get the post Data
+        if($this->request->getMethod() === "post"){
+            $data = $this->request->getVar();
+        }
+    }
+
+
+
+    
+//     $currentUserId = Session::get('loginId');
+
+//     $user= DB::table('users')
+//     ->where('users.id', '=', $currentUserId)
+//     ->select('users.*')
+//     ->first();
+// -------------------------------------------------------------------------------------------
+// public function your_account_update(Request $request)
+// {
+//     $currentUserId = Session::get('loginId');
+//     // Validate the incoming request
+//     $request->validate([
+     
+//         'email' => 'required|email|max:255',
+//         'user_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:100000',
+//     ]);
+  //return redirect()->back()->with('success', 'Account updated successfully.');
 }
+
